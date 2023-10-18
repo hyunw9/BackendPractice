c4e5686664fde47fa040e4578cafe1345cb8267b
hyunw9 <rkdgusdnr32@naver.com>
2023-10-18 21:22:51 +0900

    added : test3.cpp

---
+++ lab6/test3.cpp
@@ -0,0 +1,30 @@
+#include <chrono>
+#include <iostream>
+#include <thread>
+
+using namespace std;
+
+void f1(){
+    cout << "f1" << endl;
+    this_thread::sleep_for(chrono::milliseconds(10*1000)); //10초 동안 재우기
+    cout << "f1:woke up"<< endl;
+}
+
+void f2(int arg){
+    cout << "f2: " << arg <<endl;
+    this_thread::sleep_for(chrono::milliseconds(10*1000)); //10초 동안 재우기
+    cout << "f2:woke up"<< endl;
+}
+
+int main(){
+    thread t1;
+    thread t2(f1);
+    thread t3(f2,10);
+    cout<< "C++ id: " << t3.get_id() <<endl;
+    cout<< "Native id: " << t3.native_handle() << endl;
+    
+    t2.join(); //기다리기 
+    t3.join();
+
+    return 0;
+}
